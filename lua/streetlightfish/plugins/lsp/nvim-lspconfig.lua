2b9cfa8dcd9773babc3022a5d4533d21b9718375
ReelManifesto <brian.lesnak@gmail.com>
2024-07-20 18:37:44 -0400

    Added none-ls for formatting

--- lua/streetlightfish/plugins/lsp/nvim-lspconfig.lua
+++ lua/streetlightfish/plugins/lsp/nvim-lspconfig.lua
@@ -1,22 +1,22 @@
 return {
-  'neovim/nvim-lspconfig',
-  config = function()
-    local lspconfig = require('lspconfig')
+	"neovim/nvim-lspconfig",
+	config = function()
+		local lspconfig = require("lspconfig")
 
-    lspconfig.lua_ls.setup{}
-    lspconfig.bashls.setup{}
-    lspconfig.dockerls.setup{}
-    lspconfig.jdtls.setup{}
-    lspconfig.terraformls.setup{}
-    lspconfig.jsonls.setup{}
-    lspconfig.yamlls.setup{}
-    lspconfig.pyright.setup{}
-    lspconfig.tsserver.setup{}
-    lspconfig.cssls.setup{}
+		lspconfig.lua_ls.setup({})
+		lspconfig.bashls.setup({})
+		lspconfig.dockerls.setup({})
+		lspconfig.jdtls.setup({})
+		lspconfig.terraformls.setup({})
+		lspconfig.jsonls.setup({})
+		lspconfig.yamlls.setup({})
+		lspconfig.pyright.setup({})
+		lspconfig.tsserver.setup({})
+		lspconfig.cssls.setup({})
 
-    -- Keymaps
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-    vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, {})
-  end,
+		-- Keymaps
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+		vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
+	end,
 }
--- lua/streetlightfish/plugins/nvim-tree.lua
+++ lua/streetlightfish/plugins/nvim-tree.lua
@@ -1,71 +1,70 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    local nvimtree = require("nvim-tree")
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local nvimtree = require("nvim-tree")
 
-    -- recommended settings from nvim-tree documentation
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+		-- recommended settings from nvim-tree documentation
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    nvimtree.setup({
-      view = {
-        width = 35,
-        side = "right",
-        signcolumn = "yes",
-      },
-      -- change folder arrow icons
-      renderer = {
-        icons = {
-          web_devicons = {
-            file = {
-              enable = true,
-              color = true,
-            },
-            folder = {
-              enable = true,
-              color = true,
-            },
-          },
-          git_placement = "signcolumn",
-          glyphs = {
-            git = {
-              unstaged = "",
-              staged = "󰄵",
-              unmerged = "",
-              renamed = "➜",
-              untracked = "★",
-              deleted = "",
-              ignored = "◌",
-            },
-          },
-        },
-        indent_markers = {
-          enable = true,
-        },
-        highlight_git = "all",
-        highlight_modified = "all",
-      },
-      -- disable window_picker for
-      -- explorer to work well with
-      -- window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      filters = {
-        dotfiles = true,
-        custom = { ".DS_Store" },
-      },
-      
-    })
+		nvimtree.setup({
+			view = {
+				width = 35,
+				side = "right",
+				signcolumn = "yes",
+			},
+			-- change folder arrow icons
+			renderer = {
+				icons = {
+					web_devicons = {
+						file = {
+							enable = true,
+							color = true,
+						},
+						folder = {
+							enable = true,
+							color = true,
+						},
+					},
+					git_placement = "signcolumn",
+					glyphs = {
+						git = {
+							unstaged = "",
+							staged = "󰄵",
+							unmerged = "",
+							renamed = "➜",
+							untracked = "★",
+							deleted = "",
+							ignored = "◌",
+						},
+					},
+				},
+				indent_markers = {
+					enable = true,
+				},
+				highlight_git = "all",
+				highlight_modified = "all",
+			},
+			-- disable window_picker for
+			-- explorer to work well with
+			-- window splits
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			filters = {
+				dotfiles = true,
+				custom = { ".DS_Store" },
+			},
+		})
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-  end
+		keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+	end,
 }
