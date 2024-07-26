8d0ce0a5f16a86dfa85e6e737628d323ac7ae1db
ReelManifesto <brian.lesnak@gmail.com>
2024-07-26 10:33:15 -0400

    Changed colorscheme to onedark

--- lua/streetlightfish/plugins/colorscheme.lua
+++ lua/streetlightfish/plugins/colorscheme.lua
@@ -1,9 +1,16 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000, -- Make sure to load this before all other start plugins
-	config = function()
-		-- Load colorscheme
-		vim.cmd.colorscheme "catppuccin-mocha"
-	end,
+  {
+  "olimorris/onedarkpro.nvim",
+  priority = 1000, -- Ensure it loads first
+  config = function()
+    vim.cmd.colorscheme "onedark"
+  end,
+}
+	-- "catppuccin/nvim",
+	-- name = "catppuccin",
+	-- priority = 1000, -- Make sure to load this before all other start plugins
+	-- config = function()
+	-- 	-- Load colorscheme
+	-- 	vim.cmd.colorscheme "catppuccin-mocha"
+	-- end,
 }
